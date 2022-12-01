d713945964531d02b3932016a258ed3c111cb07d
Francois BONNIN <francois.bonnin@ynov.com>
2022-12-01 11:46:33 +0100

    commit folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+
 import './App.scss';
+import FormLogin from "./views/Login/FormLogin";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello word
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <FormLogin></FormLogin>
     </div>
   );
 }
