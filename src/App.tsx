b618fdd0eb28f75f3d2ecf2aa424f275ca8e3001
Francois BONNIN <francois.bonnin@ynov.com>
2023-01-03 00:33:57 +0100

    Add Post with WYSIG custom input

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import FormRegister from "./views/Login/FormRegister";
 import { Route, Routes } from 'react-router-dom';
 import BottomNavBar from './components/shared/BottomNavBar';
 import Profil from './views/Profile/Profil';
+import AddPost from './components/shared/AddPost';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
             <Route path="/register" element={<FormRegister />} />
             <Route path="/profil" element={<Profil />} />
+            <Route path="/add-post" element={<AddPost />} />
         </Routes>
         <BottomNavBar></BottomNavBar>
     </div>
---
+++ src/components/shared/AddPost.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import InputPost from "./InputPost";
+
+const AddPost = () => {
+
+
+    return (
+        <div>
+            <InputPost></InputPost>
+        </div>
+    )
+}
+
+export default AddPost;
